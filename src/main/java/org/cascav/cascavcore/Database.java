ed9d05cff20e1dfdd67d1f26a708571130a5fb1d
verulyrus <merwou@hotmail.com>
2023-10-23 10:25:08 +0200

    database? denk ik?

--- src/main/java/org/cascav/cascavcore/Database.java
+++ src/main/java/org/cascav/cascavcore/Database.java
@@ -1,6 +1,42 @@
 package org.cascav.cascavcore;
 
+import org.bukkit.entity.Player;
+import org.cascav.cascavcore.Player.PlayerProperties;
+import org.redisson.api.RMap;
+
+import jakarta.persistence.*;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class Database
 {
-    //je moeder trouwens
+    private static Database instance;
+    private EntityManagerFactory entityManagerFactory;
+
+    private Database() {
+        Thread.currentThread().setContextClassLoader(getClass().getClassLoader());
+        entityManagerFactory = Persistence.createEntityManagerFactory("cascav-database");
+    }
+
+    public void loadPlayerPropertiesToRedis() {
+        RMap<UUID, PlayerProperties> Loaded = Redis.GetAllPlayerProperties();
+
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+
+        Query query = entityManager.createQuery( "SELECT * FROM PlayerProperties p", PlayerProperties.class);
+        List<PlayerProperties> results = query.getResultList();
+        for(PlayerProperties p : results)
+        {
+            Loaded.put(p.PlayerUUID, p);
+        }
+
+        entityManager.getTransaction().commit();
+        entityManager.close();
+
+        Redis.UpdatePlayerProperties(Loaded);
+    }
 }
